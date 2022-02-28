7a390ca3a6d98414d3b23b17551472028548083e
Kimcarl Cacas <kimcarlcacas@yahoo.com>
2022-02-28 15:32:23 +0800

    getting registration numbers by color

--- lib/parking_lot.rb
+++ lib/parking_lot.rb
@@ -3,12 +3,22 @@ class ParkingLot
   def initialize(size)
     @slots = Array.new(size)
   end
-  
+
   def park(car:, slot_num:)
     slots[slot_num] = car
   end
 
   def leave(slot_num)
     slots[slot_num] = nil
+  end
+
+  def get_reg_numbers_by_color(color)
+    result = []
+    slots.each do |slot|
+      next unless slot
+      result << slot.reg_no if slot.color == color
+    end
+
+    result
   end
 end
